438a4670131ef102de34e841104554b7fa1bbf66
Prathmesh0411 <prathmeshrathod01@gmail.com>
2025-05-04 20:46:04 +0530

    Update DSA9.cpp

--- DSA/DSA9.cpp
+++ DSA/DSA9.cpp
@@ -1,156 +1,301 @@
-/*Department maintains a student information. The file contains roll number,
-name, division and address. Allow user to add, delete information of student.
-Display information of particular employee. If record of student does not exist
-an appropriate message is displayed. If it is, then the system displays the student
-details. Use sequential file to main the data.*/
+/*A Dictionary stores keywords and its meanings. Provide facility for adding new
+keywords, deleting keywords, updating values of any entry. Provide facility to
+display whole data sorted in ascending/ Descending order. Also find how many
+maximum comparisons may require for finding any keyword. Use Height balance
+tree and find the complexity for finding a keyword*/
 #include<iostream>
-#include<fstream>
 using namespace std;
 
-class student
+class node
 {
-	public:
-	char name[10];
-	int roll;
-		void getdata()
-		{
-			cout<<"\n Enter the roll no and name:";
-			cin>>roll>>name;
-		}
-	
-		void putdata()
-		{
-			cout<<"\n The roll no and name:";
-			cout<<roll<<"    "<<name;
-		}
-	
+public:
+    string key;
+    string meaning;
+    node *left;
+    node *right;
 };
 
-class file
+class AVL
 {
-	fstream fp;
-	public:
-		void create()
-		{
-			char ans;
-			student s;
-			fp.open("stu.dat",ios::out);
-			do
-			{
-				s.getdata();
-				fp.write((char *)&s,sizeof(s));
-				cout<<"\n More?";
-				cin>>ans;
-			}while(ans=='Y'||ans=='y');
-			fp.close();
-		}
-                void append()
-		{
-			char ans;
-			student s;
-			fp.open("stu.dat",ios::app);
-			do
-			{
-				s.getdata();
-				fp.write((char *)&s,sizeof(s));
-				cout<<"\n More?";
-				cin>>ans;
-			}while(ans=='Y'||ans=='y');
-			fp.close();
-		}
-		void display()
-		{
-			student s;
-			fp.open("stu.dat",ios::in);
-			while(!fp.eof())
-			{
-				fp.read((char *)&s,sizeof(s));
-				if (!fp.eof())
-					s.putdata();
-			}
-			fp.close();
-		}
-                void search()
-		{
-			student s; int flag=0;
-                        int r;  
-                        cout <<"\n Enter roll to be searched:";
-                        cin >> r;
-			fp.open("stu.dat",ios::in);
-			while(!fp.eof())
-			{
-				fp.read((char *)&s,sizeof(s));
-				if (!fp.eof() && s.roll==r)
-                                {  flag=1;  s.putdata(); break;    }
-			}
-                        if (flag==0)  cout << "\n Not found";
-			fp.close();
-		}
-		void update()
-		{
-			student s; int flag=0;
-                       int r;  
-                        cout <<"\n Enter roll to be updated:";
-                        cin >> r;
-			fp.open("stu.dat",ios::in|ios::out);
-			while(!fp.eof())
-			{
-				fp.read((char *)&s,sizeof(s));
-				if (!fp.eof() && s.roll==r)
-                                {  
-                                  flag=1;  
-                                  cout <<"\n Enter new data\n";
-                                  s.getdata();
-                                  fp.seekp(-1*sizeof(s),ios::cur);
-                                  fp.write((char *)&s,sizeof(s));  
-                                  break;    
-                                }
-			}
-			
-                        if (flag==0)  cout << "\n Not found";
-			fp.close();
-		}
-		void delete1()
-		{
-			student s; int flag=0;
-			fstream fp1;
-                        cout <<"\n Enter roll to be deleted:";
-                        int r;  cin >> r;
-			fp.open("stu.dat",ios::in);
-			fp1.open("temp.dat",ios::out);
-			while(fp.read((char *)&s,sizeof(s)))
-			{
-				
-				if ( s.roll!=r)
-                        {  
-                        	flag=1;
-				   	fp1.write((char *)&s,sizeof(s));
-                        }
-			}
-                        if (flag==0)  cout << "\n Not found";
-			fp.close();
-			fp1.close();
-			remove("stu.dat");
-			rename("temp.dat","stu.dat");
-		}
+    node *root;
+       public:
+               AVL()
+               {
+                   root=NULL;
+               }
+
+                void create();
+                node* insert(node *cur,node *temp);
+               node* balance(node *temp);
+               int dif(node *temp);
+               int height(node *temp);
+               int maximum(int a,int b);
+
+               node* LL(node *par);
+               node* RR(node *par);
+               node* LR(node *par);
+               node* RL(node *par);
+
+               void ascending(node *temp);
+               node* delete_n(node *root,string key1);
+               void deleten();
+
+               node* extractmin(node *t);
+               void descending(node *temp);
+               void display();
+               bool search(node *cur,string key1);
+               void search_value();
 };
 
+void AVL::create()
+{
+    char answer;
+    node *temp;
+    do
+    {
+        temp=new node();
+        cout<<endl<<"Enter keyword:\t";
+        cin>>temp->key;
+        cout<<"Enter meaning:\t";
+        cin>>temp->meaning;
+        temp->left=temp->right=NULL;
+
+            root=insert(root,temp);
+
+        cout<<endl<<"Add another word? (y/n):\t";
+        cin>>answer;
+    }
+    while(answer=='y'||answer=='Y');
+}
+
+
+node* AVL::insert(node *cur,node *temp)
+{
+    if(cur==NULL)
+    {
+        return temp;
+    }
+    if(temp->key<cur->key)
+    {
+        cur->left=insert(cur->left,temp);
+        cur=balance(cur);
+    }
+    else if(temp->key>cur->key)
+    {
+        cur->right=insert(cur->right,temp);
+        cur=balance(cur);
+    }
+    return cur;
+}
+
+node* AVL::balance(node *temp)
+{
+    int bal;
+    bal=dif(temp);
+
+    if(bal>=2)
+    {
+        if(dif(temp->left)<0)
+            temp=LR(temp);
+        else
+            temp=LL(temp);
+    }
+    else if(bal<=-2)
+    {
+        if(dif(temp->right)<0)
+            temp=RR(temp);
+        else
+            temp=RL(temp);
+    }
+    return temp;
+}
+
+
+int AVL::dif(node *temp)
+{
+    int l,r;
+    l=height(temp->left);
+    r=height(temp->right);
+    return(l-r);
+}
+
+int AVL::height(node *temp)
+{
+    if(temp==NULL)
+        return(-1);
+    else
+        return(max(height(temp->left),height(temp->right))+1);
+}
+
+int AVL::maximum(int a,int b)
+{
+    if(a>b)
+        return a;
+    else
+        return b;
+}
+
+node* AVL::LL(node *par)
+{
+    node *temp,*temp1;
+    temp=par->left;
+    temp1=temp->right;
+    temp->right=par;
+    par->left=temp1;
+    return temp;
+}
+
+node* AVL::RR(node *par)
+{
+    node *temp,*temp1;
+    temp=par->right;
+    temp1=temp->left;
+    temp->left=par;
+    par->right=temp1;
+    return temp;
+}
+
+node* AVL::LR(node *par)
+{
+    par->left=RR(par->left);
+    return(LL(par));
+}
+
+node* AVL::RL(node *par)
+{
+    par->right=LL(par->right);
+    return(RR(par));
+}
+
+void AVL::ascending(node *temp)
+{
+       if(temp!=NULL)
+       {
+               ascending(temp->left);
+               cout<<"\n\t"<<temp->key<<" : "<<temp->meaning;
+               ascending(temp->right);
+       }
+}
+
+void AVL::descending(node *temp)
+{
+       if(temp!=NULL)
+       {
+               descending(temp->right);
+               cout<<"\n\t"<<temp->key<<" : "<<temp->meaning;
+               descending(temp->left);
+       }
+}
+
+
+void AVL::display()
+{
+       cout<<endl<<"Keywords in ascending order:\t";
+       ascending(root);
+       cout<<endl<<"Keywords in descending order:\t";
+       descending(root);
+}
+
+bool AVL::search(node *cur,string key1)
+{
+    if(cur)
+    {
+        if(cur->key==key1)
+            return true;
+        if(cur->key>key1)
+            return search(cur->left,key1);
+        else
+            return search(cur->right,key1);
+    }
+    return false;
+}
+
+void AVL::search_value()
+{
+    string key2;
+       cout<<endl<<"Keyword to search:\t";
+       cin>>key2;
+       if(search(root,key2))
+               cout<<endl<<"Keyword exists in AVL tree.";
+       else
+               cout<<endl<<"Keyword does not exist in AVL tree.";
+}
+
+
+node* AVL::delete_n(node* cur,string key1)
+{
+   if ( !cur)
+       return cur;
+   if ( key1 < cur->key )
+       cur->left = delete_n(cur->left, key1);
+
+   else if( key1 > cur->key )
+       cur->right = delete_n(cur->right, key1);
+
+   else
+   {
+       node *l = cur->left;
+       node *r = cur->right;
+       delete cur;
+       if ( !r )
+           return l;
+       node *m=r;
+
+       while(m->left)
+           m=m->left;
+       m->right = extractmin(r);
+       m->left = l;
+       return balance(m);
+   }
+   return balance(cur);
+}
+
+   node* AVL::extractmin(node *t)
+   {
+       if ( !t->left )
+       return t->right;
+       t->left = extractmin(t->left);
+       return balance(t);
+   }
+
+void AVL::deleten()
+{
+    string key;
+    cout<<endl<<"Keyword to delete:\t";
+    cin>>key;
+    root=delete_n(root,key);
+}
 
 int main()
 {
-	file f; int choice;
-        do{cout<<"\n";
-           cout << "\n1.Create \n2.Display \n3.Search \n4.Append  \n5.Delete \n6.Update";
-           cout << "\n Enter choice:";  cin >> choice;
-	   cout<<"\n";
-           switch(choice)
-           {
-               case 1:f.create(); break;
-               case 2:f.display(); break;
-               case 3:f.search(); break;
-               case 4:f.append(); break;
-	       case 5:f.delete1();break;
-	       case 6:f.update();break;
-           }
-       }while(choice< 7);
+ char c;
+ int ch;
+ AVL a;
+ do
+ {
+      cout<<endl<<"--- MAIN MENU ---";
+      cout<<endl<<"1 -> Insert keyword";
+      cout<<endl<<"2 -> Display AVL tree";
+      cout<<endl<<"3 -> Search a keyword";
+      cout<<endl<<"4 -> Delete a keyword";
+      cout<<endl<<"Choose an option (1-4):\t";
+      cin>>ch;
+      switch(ch)
+      {
+           case 1 : a.create();
+              break;
+           case 2 : a.display();
+              break;
+           case 3 : a.search_value();
+              break;
+           case 4 : a.deleten();
+              break;
+           default : cout<<endl<<"Please choose a valid option (1-4).";
+      }
+      cout<<endl<<"Would you like to continue? (y/n):\t";
+      cin>>c;
+       }
+       while(c=='y'||c=='Y');
+      cout<<"\n\n// END OF CODE\n\n";
+ return 0;
 }
